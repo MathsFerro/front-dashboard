16e5c6e5ad482a9d27c0f0ec5cb2b10f4c3e99dc
Matheus Ferro <matheusferroraimundo@gmail.com>
2022-06-16 01:35:02 -0300

    animation icon

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { BooleanInput } from '@angular/cdk/coercion';
 import { Component, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
@@ -6,9 +7,29 @@ import { ItemMenu } from './models/ItemMenu';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
+  animations: [    
+    trigger('onSideNavChange', [
+      state('close',   
+        style({
+          'margin-left': '0px',
+        }) 
+      ),
+      state('open',
+        style({
+          'margin-left': '60px',
+          'transform': 'rotate(180deg)'
+        }) 
+      ),
+      transition('open <=> closed', [
+        animate('0.2s')
+      ]),
+    ])
+  ],
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+
+  public isOpen = true;
 
   public menuItems: ItemMenu[] = [];
   public selectedMenu: number;
@@ -32,5 +53,4 @@ export class SidenavComponent implements OnInit {
   handleSelectedMenu(menu: ItemMenu) {
     this.selectedMenu = menu.value;
   }
- 
 }
